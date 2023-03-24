f21759761d57a1f920743252141f5f64faf3885e
eunrose <117526702+eunrose@users.noreply.github.com>
2023-03-24 22:19:40 +0800

    first commit

---
+++ lib/constant/add_color.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFF1A1A1A);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greyShadeColor = Color(0xFFA8A8A8);
+  static const Color offWhiteColor = Color(0xFFEFEFEF);
+  static const Color blueColor = Color(0xFF0098FC);
+  static const Color greyColor = Color(0xFF363636);
+  static const Color redColor = Color(0xFF363636);
+  static const Color blackColor = Color(0xFF363636);
+
+  static const MaterialColor primaryColor = MaterialColor(0xFF6C63FF, {
+    50: Color(0xFFE1E1FF),
+    100: Color(0xFFBDBDFF),
+    200: Color(0xFF9B9BFF),
+    300: Color(0xFF7A7AFF),
+    400: Color(0xFF6C63FF),
+    500: Color(0xFF5E5EFF),
+    600: Color(0xFF5E5EFF),
+    700: Color(0xFF5E5EFF),
+    800: Color(0xFF5E5EFF),
+    900: Color(0xFF5E5EFF),
+  });
+}
